d3a0df439406d240af7b60c75614eb318d2078c4
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-03-18 10:34:41 -0700

    convert word variables in story to match ids in html and small story editing

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ wordForm.addEventListener("submit", (e) => {
     let place = document.getElementById("place").value;
     let password = document.getElementById("password").value;
     
-    let storyText = `Once there was a strange time where a <span class="word">${word01}</span> took over the world
-    and made us all stay span <span class="word">${word02}</span> at home.  So we 
-    <span class="word">${word03}</span> into our imagination and found out the key to 
-    <span class="word">${word04}</span> was all about wearing <span class="word">${word05}</span> 
-    with <span class="word">${word06}</span>.  It turned out that this was also the trick to turning 
-    on our <span class="word">${word07}</span> and getting to do everything you love all at once.  
-    Now, we can eat <span class="word">${word08}</span> while playing <span class="word">${word09}</span>, 
-    or search for <span class="word">${word10}</span> while singing <span class="word">${word11}</span>.  
-    One time, we even turned <span class="word">${word12}</span> into a <span class="word">${word13}</span>.  
-    Don’t believe us?  Just say <span class="word">" ${word14} "</span> and we’ll show you.`;
+    let storyText = `Once there was a strange time where a <span class="word">${creature}</span> took over the world
+    and made us all stay <span class="word">${adjective}</span> at home.  So we 
+    <span class="word">${verb}</span> into our imagination and found out the key to 
+    <span class="word">${feeling}</span> was all about wearing <span class="word">${clothing}</span> 
+    with <span class="word">${sparkly}</span>.  It turned out that this was also the trick to turning 
+    on our <span class="word">${superpower}</span> and getting to do everything you love all at once.  
+    Now, we can eat <span class="word">${food}</span> while playing <span class="word">${game}</span>, 
+    or search for a <span class="word">${animal}</span> while singing <span class="word">${song}</span>.  
+    One time, we even turned the <span class="word">${room}</span> into <span class="word">${place}</span>.  
+    Don’t believe us?  Just say <span class="word">" ${password} "</span> and we’ll show you.`;
   
     story.textContent = storyText;
 });
