990b18930779d39275fcea599eed306c4a23b740
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-03-25 14:47:57 -0700

    delete story json from app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const storyTitle = document.querySelector(".story-title1");
 const form = document.querySelector("form");
 const story = document.querySelector(".story")
 
-// JSON object with the story title, an array holding the labels, an array holding the 
-// ids, and an array holding the story pieces
-let json = '{"title": "The Brady Bunch","labels": ["Adjective","Singular noun","Number","Adjective","Plural of the above noun","Plural body part","Color","Plural Noun"],"inputIDs": ["adjective","singular-noun","number", "adjective-2", "plural-noun", "body-part", "color", "plural-noun-2"],"storyPieces": ["Here\'s the story of a "," ", " who was bringing up "," very ", " ", ".  All of them had ", " of ", " like their mother, the youngest one had ","."]}';
-
 // convert json object into javascript object
 let parsed = JSON.parse(json);
 
