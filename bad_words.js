3df0c437dd349415959ea47da3de94b6d852bc24
chonger878 <echong06@gmail.com>
2023-03-27 02:14:59 -0700

    Played more with bad_words.js file

--- bad_words.js
+++ bad_words.js
@@ -2,3 +2,12 @@ var Filter = require('bad-words')
 filter = new Filter();
 
 console.log(filter.clean("Don't be an ash0le"));
+
+//How about we were to create a new list of bad words instead of an API?
+var bad_words_list = ['bitch', 'fuck', 'asshole'];
+
+filter.addWords(...bad_words_list)
+
+//but this might be more cleaner especially if we need to compare
+//user data and list items
+filterTwo = new Filter({list:['damn', 'bastard', 'dick']})
